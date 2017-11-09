c25bb196aa34d08f3c3e2bf09bbf5a5e358a5c9c
ymarkov <itemruby@gmail.com>
2017-11-09 15:18:07 +0300

    add async validator

--- src/app/auth/registration/registration.component.ts
+++ src/app/auth/registration/registration.component.ts
@@ -1,46 +1,60 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {FormGroup, FormControl, Validators} from "@angular/forms";
 import {UserService} from "../../shared/services/users.service";
 import {User} from "../../shared/models/user.model";
 import {Router} from "@angular/router";
 
+
 @Component({
-  selector: 'wfm-registration',
-  templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+    selector: 'wfm-registration',
+    templateUrl: './registration.component.html',
+    styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent implements OnInit {
 
-  form: FormGroup;
+    form: FormGroup;
 
-  constructor(
-      private userService: UserService,
-      private router: Router
-  ) {}
-
-
-  ngOnInit() {
-    this.form = new FormGroup({
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
-      'name': new FormControl(null, [Validators.required]),
-      'agree': new FormControl(false, [Validators.requiredTrue])
-    })
-  }
-
-  onSubmit(){
-    const {email, password, name} = this.form.value;
-    const user = new User(email, password, name);
+    constructor(private userService: UserService,
+                private router: Router) {
+    }
 
 
-    this.userService.createNewUser(user)
-        .subscribe(data=> {
-           this.router.navigate(['/login'], {
-             queryParams : {
-               nowCanLogin: true
-             }
-           })
+    ngOnInit() {
+        this.form = new FormGroup({
+            'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails.bind(this)),
+            'password': new FormControl(null, [Validators.required, Validators.minLength(6)]),
+            'name': new FormControl(null, [Validators.required]),
+            'agree': new FormControl(false, [Validators.requiredTrue])
         })
-  }
+    }
+
+    onSubmit() {
+        const {email, password, name} = this.form.value;
+        const user = new User(email, password, name);
+
+
+        this.userService.createNewUser(user)
+            .subscribe(data => {
+                this.router.navigate(['/login'], {
+                    queryParams: {
+                        nowCanLogin: true
+                    }
+                })
+            })
+    }
+
+
+    forbiddenEmails(control: FormControl): Promise<any> {
+        return new Promise((resolve, reject) => {
+            this.userService.getUserByEmail(control.value)
+                .subscribe((user: User) => {
+                    if (user) {
+                        resolve({forbiddenEmail: true});
+                    } else {
+                        resolve(null);
+                    }
+                });
+        });
+    }
 
 }
--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -11,11 +11,12 @@ export class UserService {
 
     }
 
-    getUserByEmail(email: string): Observable<User>{
-        return this.http.get(`http://localhost:3000/users?=email=${email}`)
-            .map((response: Response)=> response.json())
-            .map((user: User[])=>  user[0]? user[0] : undefined)
+    getUserByEmail(email: string): Observable<User> {
+        return this.http.get(`http://localhost:3000/users?email=${email}`)
+            .map((response: Response) => response.json())
+            .map((user: User[]) => user[0] ? user[0] : undefined);
     }
+
 
     createNewUser(user: User){
         return this.http.post(`http://localhost:3000/users`, user)
