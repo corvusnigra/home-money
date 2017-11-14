d9bf2e035c661652f5d7c383f76e0830a5017ee6
corvusnigra <itemruby@gmail.com>
2017-11-15 00:01:42 +0300

    add header component

--- src/app/system/records-page/add-event/add-event.component.ts
+++ src/app/system/records-page/add-event/add-event.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {NgForm} from "@angular/forms";
+import {Component, Input, OnInit} from '@angular/core';
+
+import {Category} from "../../shared/models/category.model";
+import {WFMEvent} from "../../shared/models/event.model";
+import * as momemt from "moment";
 
 @Component({
   selector: 'wfm-add-event',
@@ -7,9 +12,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddEventComponent implements OnInit {
 
+  @Input() categories: Category[] = [];
+  types = [
+      {type: 'income', label: 'Доход'},
+      {type: 'outcome', label: 'Расход'}
+  ]
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: NgForm) {
+    let {type, amount, description, category} = form.value;
+
+    const event = new WFMEvent(type, amount, category, momemt().format('DD.MM.YYYY HH:mm:ss'), description);
   }
 
 }
